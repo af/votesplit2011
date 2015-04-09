80444a87b366e7ecd13f30ba92589a12b5237ee7
Aaron Franks <aaron.franks@gmail.com>
2015-04-09 09:44:45 -0700

    WIP for map zooming.

--- js/entry.js
+++ js/entry.js
@@ -3,20 +3,29 @@ let topojson = require('topojson')
 
 const WIDTH = Math.max(900, window.innerWidth);
 const HEIGHT = 600
+
+let container;
+let projection = d3.geo.albers().translate([WIDTH / 2, HEIGHT ]);
+let pathProjection = d3.geo.path().projection(projection)
+let zoom = d3.behavior.zoom().scaleExtent([1, 20])
+             .on('zoom', () => {
+                let evt = d3.event
+                container.attr('transform', `translate(${evt.translate})scale(${evt.scale})`)
+             })
+
 let svg = d3.select('body').append('svg')
             .attr('width', WIDTH)
             .attr('height', HEIGHT)
-            .append('g').attr('class', 'container')
             .attr('transform', `translate(0, ${HEIGHT - 150})`)
-let projection = d3.geo.albers()
-let pathProjection = d3.geo.path().projection(projection)
+            .call(zoom)
+container = svg.append('g').attr('class', 'container')
 
 d3.json('districts.topojson', function(error, canada) {
     if (error) return console.error(error)
     //console.log(canada, svg)
 
     var districts = topojson.feature(canada, canada.objects.gfed000b11a_e).features
-    var paths = svg.selectAll('.district').data(districts)
+    var paths = container.selectAll('.district').data(districts)
 
     var parties = 'CPC,LPC,NDP,GRN,BLC'.split(',')
     var seatTotals = {}
@@ -29,7 +38,7 @@ d3.json('districts.topojson', function(error, canada) {
             // d.properties.CPC = d.properties.CPC - split
 
             let winner = parties.map(party => ({ name: party, votes: d.properties[party] }))
-                                .reduce(((max, next) => next.votes > max.votes ? next : max),                                        { votes: 0 })
+                                .reduce(((max, next) => next.votes > max.votes ? next : max), { votes: 0 })
             seatTotals[winner.name] = seatTotals[winner.name] + 1 || 1
             return `district ${winner.name}`
         })
