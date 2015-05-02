bb148ea252060f684527d85c285fcb69c41d5bde
Aaron Franks <aaron.franks@gmail.com>
2015-05-01 22:25:59 -0700

    Misc cleanup from a few weeks ago.

--- js/entry.js
+++ js/entry.js
@@ -1,19 +1,23 @@
 let d3 = require('d3')
 let topojson = require('topojson')
 
-const WIDTH = 900
+const WIDTH = Math.max(900, window.innerWidth);
 const HEIGHT = 600
 let svg = d3.select('body').append('svg')
             .attr('width', WIDTH)
             .attr('height', HEIGHT)
+            .append('g').attr('class', 'container')
+            .attr('transform', `translate(0, ${HEIGHT - 150})`)
 let projection = d3.geo.albers()
+let pathProjection = d3.geo.path().projection(projection)
 
 d3.json('election_districts.topojson', function(error, canada) {
     if (error) return console.error(error)
     console.log(canada, svg)
 
-    svg.append('path')
-        .attr('transform', `translate(0, ${HEIGHT - 150})`)
-        .datum(topojson.feature(canada, canada.objects.election_districts))
-        .attr('d', d3.geo.path().projection(projection))
+    var districts = topojson.feature(canada, canada.objects.election_districts).features
+    var paths = svg.selectAll('.district').data(districts)
+    paths.enter().insert('path')
+        .attr('class', d => `district ${d.id}`)
+        .attr('d', pathProjection)
 });
