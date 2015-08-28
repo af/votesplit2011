373764d1ea4d83cab3fc071e46d48e0aa0afd121
Aaron Franks <aaron.franks@gmail.com>
2015-08-28 08:34:25 -0700

    WIP for zooming in to cities.

--- js/constants.js
+++ js/constants.js
@@ -6,4 +6,11 @@ const OTHER_PARTY = 'OTHER'
 let PARTIES_WITH_OTHER = PARTIES.slice()
 PARTIES_WITH_OTHER.push(OTHER_PARTY)
 
-module.exports = { PARTIES, PARTIES_WITH_OTHER, OTHER_PARTY, SEAT_COUNT }
+// Mapping of names to zoom instructions
+const ZOOM_FEATURES = {
+    Canada: { id: null, scale: 1 },
+    Montreal: { id: 24003, scale: 15 },
+    Toronto: { id: 35106, scale: 17 }
+}
+
+module.exports = { PARTIES, PARTIES_WITH_OTHER, OTHER_PARTY, SEAT_COUNT, ZOOM_FEATURES }
--- js/map.js
+++ js/map.js
@@ -3,11 +3,13 @@
 let d3 = require('d3')
 let React = require('react')
 let d = require('jsnox')(React)
+let { ZOOM_FEATURES } = require('./constants')
 
 const WIDTH = Math.max(300, window.innerWidth)
 const HEIGHT = Math.max(400, window.innerHeight)
 const SIDEBAR_WIDTH = 300           // Duplicated from CSS unfortunately
 const IS_PORTRAIT = HEIGHT > WIDTH
+const DEFAULT_TRANSLATE = [WIDTH / 2, Math.min(HEIGHT, WIDTH)]
 
 
 module.exports = React.createClass({
@@ -19,18 +21,42 @@ module.exports = React.createClass({
     },
 
     componentDidMount() {
-        let svg = d3.select(this.getDOMNode())
-        this.vizRoot = svg.append('g').attr('class', 'container')
+        this.svg = d3.select(this.getDOMNode())
+        this.vizRoot = this.svg.append('g').attr('class', 'container')
         this.projection = d3.geo.albers()
                                 .scale(IS_PORTRAIT ? WIDTH : WIDTH - SIDEBAR_WIDTH)
-                                .translate([WIDTH / 2, Math.min(HEIGHT, WIDTH)])
+                                .translate(DEFAULT_TRANSLATE)
         this.pathProjection = d3.geo.path().projection(this.projection)
 
-        svg.call(d3.behavior.zoom().scaleExtent([1, 20]).on('zoom', () => {
+        this.zoomer = d3.behavior.zoom()
+                       .translate([0,0])
+                       .size([WIDTH, HEIGHT])
+                       .center([WIDTH/2, HEIGHT/2])
+                       .scale(1)
+                       .scaleExtent([1, 20])
+        this.zoomer.on('zoom', () => {
             let evt = d3.event
             this.vizRoot.attr('transform', `translate(${evt.translate})scale(${evt.scale})`)
-        }))
+        })
+        this.svg.call(this.zoomer)
+
+        // FIXME
+        setTimeout(() => this.zoomToFeature(ZOOM_FEATURES.Toronto), 3000)
+        setTimeout(() => this.zoomToFeature(ZOOM_FEATURES.Montreal), 6000)
+
         if (this.props.districts) this.drawDistricts(this.props.districts)
+    },
+
+    zoomToFeature(zoomTarget) {
+        // WIP, see http://bl.ocks.org/mbostock/4699541
+        // TODO: handle null case
+        let d = d3.select('.id_' + zoomTarget.id).data()[0]     // Gotta be a better way...
+        let bounds = this.pathProjection.bounds(d)
+        let [x, y] = [bounds[0][0], bounds[0][1]]       // Use top-left corner for simplicity
+        let scale = zoomTarget.scale
+        let translation = [WIDTH/2 - scale*x, HEIGHT/2 - scale*y]
+        let zoomEvt = this.zoomer.translate(translation).scale(scale).event
+        this.svg.transition().duration(1000).call(zoomEvt);
     },
 
     // Don't ever re-render the svg element, but redraw the districts if the
@@ -56,7 +82,7 @@ module.exports = React.createClass({
             .attr('class', d => {
                 let isSelected = (d.properties.districtId === this.props.selectedDistrictId)
                 let selectedClass = isSelected ? 'selected' : ''
-                return `district ${d.properties.winner.name} ${selectedClass}`
+                return `district ${d.properties.winner.name} ${selectedClass} id_${d.properties.districtId}`
             })
             .on('click', function(d) {
                 let selectedId = component.selectDistrict(d.properties)
